1f4e122e849a2c74f9280dbc246705253d45ed7b
Haonan Lu <hnlu546@gmail.com>
2022-11-27 21:05:29 -0500

    add fields

--- model/booklist.js
+++ model/booklist.js
@@ -1,6 +1,11 @@
 import mongoose from "mongoose";
 
 const booklist = mongoose.Schema({
+  title: {
+    type: String,
+    require: true,
+    unique: true
+  },
   creator: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User"
