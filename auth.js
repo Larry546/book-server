cffcb6f604b669efcab0103964320db900201819
Haonan Lu <hnlu546@gmail.com>
2022-12-05 00:04:04 -0500

    change

--- auth.js
+++ auth.js
@@ -1,8 +1,8 @@
 import User from "./model/user.js"
 
 export const authUser = async (req, res, next) => {
-  if (req.session && req.session.user && req.session.user._id) {
-    const user = await User.findById(req.session.user._id);
+  if (req.session && req.session['user'] && req.session['user']._id) {
+    const user = await User.findById(req.session['user']._id);
     if (user)
       return next();
   }
